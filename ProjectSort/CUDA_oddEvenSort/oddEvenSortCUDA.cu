7417f0f5d176278083046ff2711dffb23191b42a
Rijul Ranjan <72848214+rijulrr@users.noreply.github.com>
2023-11-15 20:19:49 -0600

    Add files via upload

--- ProjectSort/CUDA_oddEvenSort/oddEvenSortCUDA.cu
+++ ProjectSort/CUDA_oddEvenSort/oddEvenSortCUDA.cu
@@ -1,5 +1,5 @@
 
-// source: 
+// source:
 
 #include <stdio.h>
 #include <cuda.h>
@@ -7,61 +7,106 @@
 #include <cstdlib>
 #include <ctime>
 
-__global__ void oddeven(int* x, int I, int n) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (I == 0 && id < n / 2) {
-        int ix = id * 2;
-        if (x[ix] > x[ix + 1]) {
-            int temp = x[ix];
-            x[ix] = x[ix + 1];
-            x[ix + 1] = temp;
+__device__ inline void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+__global__ void oddEvenSort(int *a, int n, bool *sorted)
+{
+    __shared__ int isSorted;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for (int phase = 0; phase < n; ++phase)
+    {
+        isSorted = 1;
+        __syncthreads();
+
+        // Even phase
+        if (phase % 2 == 0)
+        {
+            if (idx < n / 2)
+            {
+                int i = idx * 2;
+                if (a[i] > a[i + 1])
+                {
+                    swap(a[i], a[i + 1]);
+                    isSorted = 0;
+                }
+            }
         }
-    }
-    else if (I == 1 && id < (n - 1) / 2) {
-        int ix = id * 2 + 1;
-        if (x[ix] > x[ix + 1]) {
-            int temp = x[ix];
-            x[ix] = x[ix + 1];
-            x[ix + 1] = temp;
+        // Odd phase
+        else
+        {
+            if (idx < (n - 1) / 2)
+            {
+                int i = idx * 2 + 1;
+                if (a[i] > a[i + 1])
+                {
+                    swap(a[i], a[i + 1]);
+                    isSorted = 0;
+                }
+            }
         }
+
+        __syncthreads();
+        
+        // Check if the array is sorted
+        if (isSorted == 0)
+            *sorted = false;
     }
 }
 
-void printArray(int *array, int size) {
-    for (int i = 0; i < size; i++) {
+
+
+void printArray(int *array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         std::cout << array[i] << " ";
     }
     std::cout << std::endl;
 }
 
-int main() {
+int main(int argc, char** argv)
+{
     int *a, *d, n, *c;
     int threadsPerBlock;
 
-    std::cout << "Enter the number of elements to sort: ";
-    std::cin >> n;
-    std::cout << "Enter the number of threads per block: ";
-    std::cin >> threadsPerBlock;
+    n = atoi(argv[1]);
+    threadsPerBlock = atoi(argv[2]);
 
     a = new int[n];
     c = new int[n];
 
     srand(time(0));
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         a[i] = rand() % 1000;
     }
 
-	std::cout << "Unsorted Array is: ";
+    std::cout << "Unsorted Array is: ";
     printArray(a, n);
 
-    cudaMalloc((void**)&d, n * sizeof(int));
+    cudaMalloc((void **)&d, n * sizeof(int));
     cudaMemcpy(d, a, n * sizeof(int), cudaMemcpyHostToDevice);
 
     int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
-    for (int i = 0; i < n; i++) {
-        oddeven<<<blocksPerGrid, threadsPerBlock>>>(d, i % 2, n);
-        cudaDeviceSynchronize(); // Ensure each stage of the sort is complete before starting the next
+    bool sorted = false;
+    bool *d_sorted;
+    cudaMalloc((void **)&d_sorted, sizeof(bool));
+
+    while (!sorted)
+    {
+        sorted = true;
+        cudaMemcpy(d_sorted, &sorted, sizeof(bool), cudaMemcpyHostToDevice);
+        
+        oddEvenSort<<<blocksPerGrid, threadsPerBlock>>>(d, n, d_sorted);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(&sorted, d_sorted, sizeof(bool), cudaMemcpyDeviceToHost);
     }
 
     cudaMemcpy(c, d, n * sizeof(int), cudaMemcpyDeviceToHost);
@@ -70,8 +115,9 @@ int main() {
     printArray(c, n);
 
     cudaFree(d);
-    delete[] a;
-    delete[] c;
+    cudaFree(a);
+    cudaFree(c);
 
     return 0;
 }
+
