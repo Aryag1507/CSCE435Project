71f08d03133456a6b6e6984a1fe3beb0f898311d
Arya Gupta <aryarnm@gmail.com>
2023-11-15 20:19:17 -0600

    mergeSortCuda

--- ProjectSort/CUDA_mergeSort/mergeSortCUDA.cu
+++ ProjectSort/CUDA_mergeSort/mergeSortCUDA.cu
@@ -2,16 +2,17 @@
 // source: ChatGPT
 
 #include <iostream>
-#include <cuda.h>
+#include <cuda_runtime.h>
 #include <cstdlib>
 #include <ctime>
-#include <caliper/cali.h>
+
+#define MAX_WIDTH 512
 
 const char* mem_alloc = "mem_alloc";
 const char* sorting = "sorting";
 const char* whole_computation = "whole_computation";
 
-__global__ void simpleMerge(int *d_array, int size, int width) {
+__global__ void merge(int* d_data, int size, int width) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int start = idx * width * 2;
 
@@ -23,85 +24,73 @@ __global__ void simpleMerge(int *d_array, int size, int width) {
 
     // Merge the two sorted subarrays into temp
     while (i < middle && j < end) {
-        if (d_array[i] < d_array[j]) {
-            temp[k++] = d_array[i++];
+        if (d_data[i] < d_data[j]) {
+            temp[k++] = d_data[i++];
         } else {
-            temp[k++] = d_array[j++];
+            temp[k++] = d_data[j++];
         }
     }
 
     // Copy the remaining elements of the left subarray if there are any
     while (i < middle) {
-        temp[k++] = d_array[i++];
+        temp[k++] = d_data[i++];
     }
     // Copy the remaining elements of the right subarray if there are any
     while (j < end) {
-        temp[k++] = d_array[j++];
+        temp[k++] = d_data[j++];
     }
 
     // Copy the merged subarray back into the original array
     for (i = start, k = 0; i < end; i++, k++) {
-        d_array[i] = temp[k];
+        d_data[i] = temp[k];
     }
 
     delete[] temp;
 }
 
-void printArray(int *array, int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
-int main() {
-
-
-    int n;
-    int threadsPerBlock;
-
-    std::cout << "Enter the number of elements (must be a power of 2): ";
-    std::cin >> n;
-
-    std::cout << "Enter the number of threads per block: ";
-    std::cin >> threadsPerBlock;
-
-    CALI_MARK_BEGIN(whole_computation);
-
-    int *h_array = new int[n];
-
-    std::srand(static_cast<unsigned int>(std::time(0)));
-    for (int i = 0; i < n; i++) {
-        h_array[i] = std::rand() % 1000;
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Usage: %s <n> <num_threads>\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    std::cout << "Unsorted array: \n";
-    printArray(h_array, n);
+    int n = atoi(argv[1]);
+    int num_threads = atoi(argv[2]);
 
-    CALI_MARK_BEGIN(mem_alloc);
-    int *d_array;
-    cudaMalloc(&d_array, n * sizeof(int));
-    cudaMemcpy(d_array, h_array, n * sizeof(int), cudaMemcpyHostToDevice);
-    CALI_MARK_END(mem_alloc);
+    int* h_data = (int*)malloc(n * sizeof(int));
 
-    CALI_MARK_BEGIN(sorting);
+    // Initialize the array with random values
+    for (int i = 0; i < n; ++i) {
+        h_data[i] = rand();
+    }
+
+    // Display the original array
+    printf("Original Array:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", h_data[i]);
+    }
+    printf("\n");
+
+    int* d_data;
+    cudaMalloc((void**)&d_data, n * sizeof(int));
+    cudaMemcpy(d_data, h_data, n * sizeof(int), cudaMemcpyHostToDevice);
+
     for (int width = 1; width < n; width *= 2) {
-        int blocks = (n + threadsPerBlock - 1) / (threadsPerBlock * 2);
-        simpleMerge<<<blocks, threadsPerBlock>>>(d_array, n, width);
+        int blocks = (n + (2 * num_threads) - 1) / (2 * num_threads);
+        merge<<<blocks, num_threads>>>(d_data, n, width);
         cudaDeviceSynchronize();
     }
-    CALI_MARK_END(sorting);
 
-    cudaMemcpy(h_array, d_array, n * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_data, d_data, n * sizeof(int), cudaMemcpyDeviceToHost);
 
     std::cout << "Sorted array: \n";
-    printArray(h_array, n);
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", h_data[i]);
+    }
+    printf("\n");
 
-    delete[] h_array;
-    cudaFree(d_array);
-
-    CALI_MARK_BEGIN(whole_computation);
-
+    free(h_data);
+    cudaFree(d_data);
 
     return 0;
 }
