ec226cf32bed4e6ae721f800c186493e759af253
JP Pham <63124165+JPham-file@users.noreply.github.com>
2023-11-15 21:29:39 -0600

    finished bucket

--- ProjectSort/MPI_BucketSort/bucketsort.cpp
+++ ProjectSort/MPI_BucketSort/bucketsort.cpp
@@ -4,6 +4,14 @@
 #include <algorithm>
 using namespace std;
 
+enum ArrayType
+{
+    RANDOM,
+    SORTED,
+    REVERSE_SORTED,
+    PERTURBED
+};
+
 // Function to sort arr[] of size n using bucket sort in parallel
 void bucketSort(float arr[], int n, int rank, int size) {
     // Calculate the range of data for each process
@@ -35,37 +43,77 @@ void bucketSort(float arr[], int n, int rank, int size) {
     }
 }
 
-int main(int argc, char *argv[]) {
+void generateArray(float arr[], int arraySize, ArrayType type, int rank)
+{
+    srand(time(NULL) + rank); // Seed the random number generator differently for each process
+
+    switch (type)
+    {
+    case RANDOM:
+        for (int i = 0; i < arraySize; i++)
+        {
+            arr[i] = static_cast<float>(rand()) / RAND_MAX;
+        }
+        break;
+    case SORTED:
+        for (int i = 0; i < arraySize; i++)
+        {
+            arr[i] = static_cast<float>(i) / arraySize;
+        }
+        break;
+    case REVERSE_SORTED:
+        for (int i = 0; i < arraySize; i++)
+        {
+            arr[i] = static_cast<float>(arraySize - i) / arraySize;
+        }
+        break;
+    case PERTURBED:
+        for (int i = 0; i < arraySize; i++)
+        {
+            arr[i] = static_cast<float>(i) / arraySize + static_cast<float>(rand()) / (RAND_MAX * 100);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+int main(int argc, char *argv[])
+{
     MPI_Init(&argc, &argv);
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (argc != 3) {
-        if (rank == 0) {
-            cout << "Usage: mpiexec -n <number_of_processors> ./bucketsort <array_size> <number_of_processors>" << endl;
+    if (argc != 4)
+    {
+        if (rank == 0)
+        {
+            cout << "Usage: mpiexec -n <number_of_processors> ./bucketsort <array_size> <number_of_processors> <array_type>" << endl;
+            cout << "Array types: 0 (Random), 1 (Sorted), 2 (Reverse Sorted), 3 (Perturbed)" << endl;
         }
         MPI_Finalize();
         return 1;
     }
 
-    int arraySize = stoi(argv[1]);
-    int numProcessors = stoi(argv[2]);
+    int arraySize = atoi(argv[1]);
+    int numProcessors = atoi(argv[2]);
+    ArrayType type = static_cast<ArrayType>(atoi(argv[3]));
 
     float *arr = new float[arraySize];
 
-    // Initialize the array with random values (you can use your own randomization logic here)
-    for (int i = 0; i < arraySize; i++) {
-        arr[i] = static_cast<float>(rand()) / RAND_MAX;
-    }
+    // Generate array based on user's choice
+    generateArray(arr, arraySize, type, rank);
 
     bucketSort(arr, arraySize, rank, size);
 
     // Print sorted array from rank 0
-    if (rank == 0) {
+    if (rank == 0)
+    {
         cout << "Sorted array is \n";
-        for (int i = 0; i < arraySize; i++) {
+        for (int i = 0; i < arraySize; i++)
+        {
             cout << arr[i] << " ";
         }
         cout << endl;
