61f8e8ade6853facc17b704518d8bb9bd9bd9fc9
JP Pham <63124165+JPham-file@users.noreply.github.com>
2023-11-27 20:17:13 -0600

    mergesort kinda work

--- ProjectSort/CUDA_mergeSort/mergesort.cu
+++ ProjectSort/CUDA_mergeSort/mergesort.cu
@@ -12,6 +12,40 @@
 #include <caliper/cali-manager.h>
 #include <adiak.hpp>
 
+const char* mainn = "main";
+const char* data_init = "data_init";
+const char* comm = "comm";
+const char* MPIBarrier = "MPI_Barrier";
+const char* comm_small = "comm_small";
+const char* comm_large = "comm_large";
+const char* MPIRecv = "MPI_Recv";
+const char* MPISend = "MPI_Send";
+
+const char* cuda_memcpy = "cudaMemcpy";
+
+const char* comp = "comp";
+const char* comp_small = "comp_small";
+const char* comp_large = "comp_large";
+const char* correctness_check = "correctness_check";
+
+
+/**
+ * Prints the contents of an array.
+ * 
+ * @param array Pointer to the array.
+ * @param size The number of elements in the array.
+ */
+void print_array(const float *array, int size) {
+    printf("Array Contents: [");
+    for (int i = 0; i < size; i++) {
+        printf("%f", array[i]);
+        if (i < size - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
 // Assuming array_fill function is defined elsewhere
 void array_fill_random(float *array, int size) {
     // Seed the random number generator to get different results each run
@@ -20,7 +54,10 @@ void array_fill_random(float *array, int size) {
     for (int i = 0; i < size; i++) {
         // Generate a random float between 0 and 1
         array[i] = (float)rand() / RAND_MAX;
+        // printf("%f ", array[i]);
     }
+    // printf("\n");
+    // printf("\n");
 }
 
 // Fill array in sorted order
@@ -56,24 +93,6 @@ bool is_sorted(float *array, int size) {
     return true;
 }
 
-void adiak_stuff() {
-    adiak::init(NULL);
-    adiak::launchdate();    // launch date of the job
-    adiak::libraries();     // Libraries used
-    adiak::cmdline();       // Command line used to launch the job
-    adiak::clustername();   // Name of the cluster
-    adiak::value("Algorithm", "MergeSort"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
-    adiak::value("ProgrammingModel", "CUDA"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
-    adiak::value("Datatype", "float"); // The datatype of input elements (e.g., double, int, float)
-    adiak::value("SizeOfDatatype", sizeof(float)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
-    // adiak::value("InputSize", array_size); // The number of elements in input dataset (1000)
-    adiak::value("InputType", "Random"); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
-    // adiak::value("num_procs", size); // The number of processors (MPI ranks)
-    adiak::value("group_num", 2); // The number of your group (integer, e.g., 1, 10)
-    adiak::value("implementation_source", "AI"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
-}
-
-
 int THREADS;
 int BLOCKS;
 int NUM_VALS;
@@ -87,12 +106,13 @@ __global__ void mergeKernel(float *dev_values, int size, int mid, int upper) {
     int start2 = idx > mid ? idx : mid;
     int end1 = mid, end2 = upper;
 
-    // Calculate the size needed for the temporary array in shared memory
-    int tempSize = upper - start1;
+    // Temporary array for merged result
     extern __shared__ float temp[];
 
-    // Merge the two halves into temp
-    int i = start1, j = start2, k = 0;
+    // Indexes for traversing
+    int i = start1, j = start2, k = start1;
+
+    // Merge the two halves
     while (i < end1 && j < end2) {
         if (dev_values[i] < dev_values[j]) {
             temp[k++] = dev_values[i++];
@@ -112,37 +132,33 @@ __global__ void mergeKernel(float *dev_values, int size, int mid, int upper) {
     }
 
     // Copy back the merged elements to the original array
-    k = 0;
-    for (i = start1; i < upper; i++) {
-        dev_values[i] = temp[k++];
+    for (i = start1; i < k; i++) {
+        dev_values[i] = temp[i];
     }
 }
-
 
 void mergeSort(float *dev_values, int size) {
     dim3 threads(THREADS, 1);
     dim3 blocks(BLOCKS, 1);
 
-    // Calculate shared memory size per block
-    int sharedMemSize = size * sizeof(float);
-    
-    CALI_MARK_BEGIN("comp");
-    CALI_MARK_BEGIN("comp_large");
+    // CALI_MARK_BEGIN(comp);
+    // CALI_MARK_BEGIN(comp_large);
     for (int width = 1; width < size; width *= 2) {
+        int sharedMemSize = width * 2 * sizeof(float);
         for (int i = 0; i < size; i = i + 2 * width) {
-            mergeKernel<<<blocks, threads, sharedMemSize>>>(dev_values, size, i, min(i + 2 * width, size));
-            // Check for errors after kernel launch
+            mergeKernel<<<blocks, threads, sharedMemSize>>>(dev_values, size, i + width, min(i + 2 * width, size));
             cudaError_t err = cudaGetLastError();
             if (err != cudaSuccess) {
-                fprintf(stderr, "CUDA Error: %s\n", cudaGetErrorString(err));
+                fprintf(stderr, "CUDA Error in mergeKernel: %s\n", cudaGetErrorString(err));
                 exit(EXIT_FAILURE);
             }
+            cudaDeviceSynchronize();
         }
-        cudaDeviceSynchronize();
     }
-    CALI_MARK_END("comp_large");
-    CALI_MARK_END("comp");
+    // CALI_MARK_END(comp_large);
+    // CALI_MARK_END(comp);
 }
+
 
 int main(int argc, char *argv[]) {
     // Parse command line arguments
@@ -156,7 +172,6 @@ int main(int argc, char *argv[]) {
     BLOCKS = (NUM_VALS + THREADS - 1) / THREADS;
     char* inputType = argv[3]; // New argument for input type
 
-
     printf("Number of threads: %d\n", THREADS);
     printf("Number of values: %d\n", NUM_VALS);
     printf("Number of blocks: %d\n", BLOCKS);
@@ -168,15 +183,20 @@ int main(int argc, char *argv[]) {
 
     // Allocate host array
     float *values = (float*) malloc(NUM_VALS * sizeof(float));
-    CALI_MARK_BEGIN("main");
 
     // Allocate device memory
     float *dev_values;
     cudaMalloc((void**) &dev_values, NUM_VALS * sizeof(float));
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "CUDA Error in cudaMalloc: %s\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 
     for (int run = 0; run < numRuns; ++run) {
 
         // Fill the array based on input type
+        CALI_MARK_BEGIN(data_init);
         if (strcmp(inputType, "Sorted") == 0) {
             array_fill_sorted(values, NUM_VALS);
         }
@@ -193,16 +213,27 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "Invalid input type!\n");
             exit(EXIT_FAILURE);
         }
+        CALI_MARK_END(data_init);
+        // if (run == 0) {
+            // print_array(values, NUM_VALS);
+        // }
+
 
         // Copy data from host to device
+        CALI_MARK_BEGIN(comm);
+        CALI_MARK_BEGIN(comm_large);
+        CALI_MARK_BEGIN(cuda_memcpy);
         cudaMemcpy(dev_values, values, NUM_VALS * sizeof(float), cudaMemcpyHostToDevice);
+        CALI_MARK_END(cuda_memcpy);
+        CALI_MARK_END(comm_large);
+        CALI_MARK_END(comm);
 
         cudaEvent_t start, stop;
         cudaEventCreate(&start);
         cudaEventCreate(&stop);
 
         cudaEventRecord(start);
-        mergeSort(dev_values, NUM_VALS);
+        mergeSort(dev_values, NUM_VALS);  // already has CALI mark in function
         cudaEventRecord(stop);
         cudaEventSynchronize(stop);
 
@@ -216,6 +247,10 @@ int main(int argc, char *argv[]) {
 
         // Copy sorted data back to host and verify
         cudaMemcpy(values, dev_values, NUM_VALS * sizeof(float), cudaMemcpyDeviceToHost);
+
+        // if (run == 0) {
+        //     print_array(values, NUM_VALS);
+        // }
         if (!is_sorted(values, NUM_VALS)) {
             fprintf(stderr, "Array is not correctly sorted!\n");
             exit(EXIT_FAILURE);
@@ -224,7 +259,6 @@ int main(int argc, char *argv[]) {
 
     float average = total / numRuns;
 
-    CALI_MARK_END("main");
 
     // Adiak value updates
     adiak::init(NULL);
@@ -236,9 +270,10 @@ int main(int argc, char *argv[]) {
     adiak::value("ProgrammingModel", "CUDA"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
     adiak::value("Datatype", "float"); // The datatype of input elements (e.g., double, int, float)
     adiak::value("SizeOfDatatype", sizeof(float)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
-    // adiak::value("InputSize", array_size); // The number of elements in input dataset (1000)
+    adiak::value("InputSize", NUM_VALS); // The number of elements in input dataset (1000)
     adiak::value("InputType", "Random"); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
-    // adiak::value("num_procs", size); // The number of processors (MPI ranks)
+    adiak::value("num_threads", THREADS); // The number of CUDA or OpenMP threads
+    adiak::value("num_blocks", BLOCKS); // The number of CUDA blocks 
     adiak::value("group_num", 2); // The number of your group (integer, e.g., 1, 10)
     adiak::value("implementation_source", "AI"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
 
